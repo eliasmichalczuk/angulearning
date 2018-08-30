79499f09402fd12cd7bfa27291a336adb7126ce3
ELIAS MICHALCZUK RIBEIRO DA SILVA <silva.elias@totvs.com.br>
2018-08-30 16:58:04 -0300

    MVC5

---
+++ repos/CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,30 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categoria);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ repos/CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,71 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            //dao.Lista();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDAO = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDAO.Lista();
+            ViewBag.Categorias = categorias;
+            ViewBag.Produto = new Produto();
+
+            return View();
+        }
+
+        //public ActionResult Adiciona(string nome, float preco, string descricao, int quantidade, int categoria)
+        //{
+        //    Produto produto = new Produto()
+        //    {
+        //        Nome = nome,
+        //        Preco = preco,
+        //        Descricao = descricao,
+        //        Quantidade = quantidade,
+        //        CategoriaId = categoria
+        //    };
+        //    ProdutosDAO dao = new ProdutosDAO();
+        //    dao.Adiciona(produto);
+        //    return View();
+        //}
+        [HttpPost]//so aceita envios por  post, nao exibe dados na url
+        public ActionResult Adiciona(Produto produto)
+        {
+            int idDaInformatica = 1;
+            if(produto.CategoriaId.Equals(idDaInformatica) && produto.Preco < 100)
+            {
+                ModelState.AddModelError("produto.Invalido","Informatica com preço abaixo de 100 reais");
+            }
+            if (ModelState.IsValid)
+            {
+                ProdutosDAO dao = new ProdutosDAO();
+                dao.Adiciona(produto);
+                return RedirectToAction("Index", "Produto"); //redirect com view e controller
+                                                             //return View();
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO categoriasDAO = new CategoriasDAO();
+                ViewBag.Categorias = categoriasDAO.Lista();
+                return View("Form");
+            }
+        }
+    }
+}
