d4f646a53c11b52796326399a56f71e1d0547804
ELIAS MICHALCZUK RIBEIRO DA SILVA <08703076903@edu.udesc.br>
2018-09-02 23:49:46 -0300

    ef core db pre existente 1

---
+++ repos/Alura.Filmes/Alura.Filmes.App/Dados/AluraFilmesContexto.cs
@@ -0,0 +1,41 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Dados
+{
+    class AluraFilmesContexto : DbContext
+    {//o nome escolhido na propriedade será o nome da coluna
+        public DbSet<Ator> Atores { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)mssqllocaldb;Database=AluraFilmes;Trusted_connection=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //representa o mapeamento
+        {//fluent api
+            modelBuilder.Entity<Ator>()
+                .ToTable("actor");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.Id)
+                .HasColumnName("actor_id");
+
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.PrimeiroNome)
+                .HasColumnName("first_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+            //isola toda a configuração no OnModelCreating
+            modelBuilder.Entity<Ator>()
+                .Property(a => a.UltimoNome)
+                .HasColumnName("last_name")
+                .HasColumnType("varchar(45)")
+                .IsRequired();
+        }
+    }
+}
---
+++ repos/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    //[Table("actor")]
+    //convensão do entity
+    //quebra regra de nome de tabela
+    //padrão string vira nvarchar
+    //string aceita null padrão clr
+    public class Ator
+    {
+        //[Column("actor_id")]
+        public int Id { get; set; }
+        //[Required]
+        //[Column("first_name", TypeName ="varchar(45)")]
+        public string PrimeiroNome { get; set; }
+        //[Required]
+        //[Column("last_name", TypeName = "varchar(45)")]
+        public string UltimoNome { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ator ({Id}): {PrimeiroNome} {UltimoNome}";
+        }
+    }
+}
